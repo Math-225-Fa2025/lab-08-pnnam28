7512534c01f1b9b544adc1eb7b009180517f25a3
Nam Pham <phamnamnhatnguyen123@gmail.com>
2025-12-05 00:13:19 +0000

    scrape page function

--- scripts/02-scrape-page-function.R
+++ scripts/02-scrape-page-function.R
@@ -1,27 +1,35 @@
 # load packages ----------------------------------------------------------------
-
-#library(tidyverse)
-#library(rvest)
-#
+library(tidyverse)
+library(rvest)
+library(stringr)
 ## function: scrape_page --------------------------------------------------------
-#
-#___ <- function(url){
-#  
-#  # read page
-#  page <- read_html(url)
-#  
-#  # scrape titles
-#  titles <- ___
-#  
-#  # scrape links
-#  links <- ___
-#  
-#  # scrape artists 
-#  artists <- ___
-#  
-#  # create and return tibble
-#  tibble(
-#    ___
-#  )
-#  
-#}
+
+scrape_page <- function(url) {
+  page <- read_html(url)
+  
+  titles <- page %>%
+    html_nodes(".iteminfo") %>%
+    html_node("h3 a") %>%
+    html_text() %>%
+    str_squish()
+  
+  links <- page %>%
+    html_nodes(".iteminfo") %>%
+    html_node("h3 a") %>%
+    html_attr("href") %>%
+    str_replace("^\\.", "https://collections.ed.ac.uk")
+  
+  artists <- page %>%
+    html_nodes(".iteminfo") %>%
+    html_node("p a") %>%  # adjust selector if needed
+    html_text() %>%
+    str_squish()
+  
+  tibble(
+    title  = titles,
+    artist = artists,
+    link   = links
+  )
+}
+scrape_page(first_url)
+scrape_page(second_url)
