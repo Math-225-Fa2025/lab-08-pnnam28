ca45d2413dc83ce80a343f6385c4c38e09595048
Nam Pham <phamnamnhatnguyen123@gmail.com>
2025-12-05 00:09:45 +0000

    Commit 1

--- scripts/01-scrape-page-one.R
+++ scripts/01-scrape-page-one.R
@@ -1,54 +1,80 @@
 # load packages ----------------------------------------------------------------
 
-#library(tidyverse)
-#library(rvest)
+library(tidyverse)
+library(rvest)
 #
 ## set url ----------------------------------------------------------------------
 #
-#first_url <- "https://collections.ed.ac.uk/art/search/*:*/Collection:%22edinburgh+college+of+art%7C%7C%7CEdinburgh+College+of+Art%22?offset=0"
+first_url <- "https://collections.ed.ac.uk/art/search/*:*/Collection:%22edinburgh+college+of+art%7C%7C%7CEdinburgh+College+of+Art%22?offset=0"
+
 #
 ## read first page --------------------------------------------------------------
 #
-#page <- read_html(first_url)
+page <- read_html(first_url)
 #
 ## scrape titles ----------------------------------------------------------------
 #
-#titles <- page %>%
-#  html_nodes(".iteminfo") %>%
-#  html_node("h3 a") %>%
-#  html_text() %>%
-#  ___()
+titles <- page %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_text() %>%
+  str_squish()
 #
 ## scrape links -----------------------------------------------------------------
 #
-#links <- page %>%
-#  html_nodes(".iteminfo") %>%
-#  html_node("h3 a") %>%
-#  html_attr("href") %>%
-#  str_replace("\\.", "___")
+links <- page %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_attr("href") %>%
+  # turn "./record/xxxx" into full URL
+  str_replace("^\\.", "https://collections.ed.ac.uk")
 #
 ## scrape artists ---------------------------------------------------------------
 #
-#artists <- page %>%
-#  html_nodes(".iteminfo") %>%
-#  html_node(".artist") %>%
-#  ___
+artists <- page %>%
+  html_nodes(".iteminfo") %>%
+  html_node("p a") %>%     
+  html_text() %>%
+  str_squish()
 #
 ## put together in a data frame -------------------------------------------------
 #
-#first_ten <- tibble(
-#  title = ___,
-#  artist = ___,
-#  link = ___
-#)
+first_ten <- tibble(
+  title  = titles,
+  artist = artists,
+  link   = links
+)
+first_ten
 #
 ## scrape second ten paintings --------------------------------------------------
 #
-#second_url <- "___"
+second_url <- "https://collections.ed.ac.uk/art/search/*:*/Collection:%22edinburgh+college+of+art%7C%7C%7CEdinburgh+College+of+Art%22?offset=10"
 #
-#page <- read_html(second_url)
+page2 <- read_html(second_url)
 #...
 #
-#second_ten <- tibble(
-#  ...
+titles2 <- page2 %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_text() %>%
+  str_squish()
+
+links2 <- page2 %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_attr("href") %>%
+  str_replace("^\\.", "https://collections.ed.ac.uk")
+
+artists2 <- page2 %>%
+  html_nodes(".iteminfo") %>%
+  html_node("p a") %>%   # again, confirm with SelectorGadget
+  html_text() %>%
+  str_squish()
+
+second_ten <- tibble(
+  title  = titles2,
+  artist = artists2,
+  link   = links2
+)
+second_ten
 #)
